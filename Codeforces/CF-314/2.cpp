55b6b18819d42d6306c7260a1dee129a9ba43da8
jinankjain <jinank94@dhcp-185-153.mpi-klsb.mpg.de>
2015-08-06 16:41:25 +0530

    Codeforces Added

---
+++ Codeforces/CF-314/2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int v[1000017]={0};
+int main()
+{
+	int n;
+	cin>>n;
+	char a;
+	int t;
+	int cnt =0,ans=0;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a>>t;
+		if(a=='-' && v[j]==0){ans++;}
+		else if(a=="+" && v[j]==0){v[j]=1;}
+		else if(a=="-" && v[j]==1)
+	}
+	return 0;
+}
