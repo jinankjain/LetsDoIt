68930d0e5d2670f19c5166e1bfa672c04928ba07
jinankjain <jinank94@dhcp-185-153.mpi-klsb.mpg.de>
2015-08-09 20:23:19 +0530

    Project Euler

---
+++ Graph/CC-KMHAMHA.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	int n;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		vector<int> G[n];
+		int x,y;
+		set<int> s1,s2;
+		for(int i=0;i<n;i++)
+		{
+			cin>>x>>y;
+		}
+	}
+	return 0;
+}
---
+++ NumberTheory/HR-PE13.py
@@ -0,0 +1,8 @@
+n = int(raw_input())
+sumi  = 0
+for i in range(0,n):
+	t = int(raw_input())
+	sumi = sumi + t
+
+s = str(sumi)
+print s[0:10]
